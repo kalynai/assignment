5db03d71eb8f947b663bbeb42672e2c3f343a2c0
kalyani <kalyanilammatha@gmail.com>
2023-02-14 21:13:15 -0800

    Updted with conflit changes

--- test.java
+++ test.java
@@ -1,11 +1,6 @@
 home work to create two feature branches and merge and resolve the conflict
 
-<<<<<<< HEAD
-feature 1 Branch one
-
-Need to test feature 1 on new repo calles Assignment
-=======
-<<<<<<< HEAD
+< HEAD
 feature 1 (branch)
 >>>>>>> 590d48cd396d271f8cb94c1fb04bb9038ecc4240
 
